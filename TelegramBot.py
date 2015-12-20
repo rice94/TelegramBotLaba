79b09d645eba446b66330c71f5978cd243e68db3
rice94 <fasa94.94@mail.ru>
2015-12-20 22:46:10 +0300

    Update TelegramBot.py

--- TelegramBot.py
+++ TelegramBot.py
@@ -5,38 +5,28 @@ import os
 
 token='158181658:AAHLsQ6HOVS4k30TtXrdTbfLAK0jF8ED5qw'
 bot=telebot.TeleBot(token)
-i = 0
-len=-1
 
-shutil.copy2('/var/log/auth.log','/home/groot/Desktop/log')
-f=open('/home/groot/Desktop/log')
+shutil.copy2('/var/log/auth.log','/home/kris/log')
+f=open('/home/kris/log')
 for k in f.readlines():
-    len=len+1
     linee=k
-    print (len)
 st=linee
 f.close()
+id=0
 
 @bot.message_handler(func=lambda message: True,content_types=['text'])
 def echo_msg(message):
     bot.send_message(message.chat.id,message.text)
     print (message.chat.id)
+    global id=message.chat.id
     bot.stop_polling()
-    #id=message.chat.id
-
-#@bot.message_handler()
-#def nothing(message):
-   # print ('yy')
-    #time.sleep(5)
-    #bot.stop_polling()
-
+    
 
 if __name__=='__main__':
     bot.polling()
     while(1):
-        shutil.copy2('/home/groot/Desktop/log','/home/groot/Desktop/log_old')
-        shutil.copy2('/var/log/auth.log','/home/groot/Desktop/log')
-        fb=open('/home/groot/Desktop/log')
+        shutil.copy2('/var/log/auth.log','/home/kris/log')
+        fb=open('/home/kris/log')
         flag_vivoda=0
         stroka=''
         for k in fb.readlines():
@@ -49,8 +39,8 @@ if __name__=='__main__':
 
         stroka='test_log_string'
         fb.close()
-        os.remove('/home/groot/Desktop/log')
-        fs=open('/home/groot/Desktop/log','w')
+        os.remove('/home/kris/log')
+        fs=open('/home/kris/log','w')
         print (stroka)
         if stroka != '':
             bot.send_message(-44485918,stroka)
